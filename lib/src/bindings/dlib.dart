a49d9ad6febf355a024745c01b429cebccfe9541
kienmt <>
2023-05-09 13:42:01 +0700

    fix version

--- lib/src/bindings/dlib.dart
+++ lib/src/bindings/dlib.dart
@@ -1,6 +1,8 @@
 import 'dart:ffi';
 import 'dart:io';
+import 'dart:math';
 
+import 'package:flutter/services.dart';
 import 'package:path/path.dart' as Path;
 
 const Set<String> _supported = {'linux', 'mac', 'win'};
@@ -31,12 +33,39 @@ String get binaryName {
 // ignore: missing_return
 DynamicLibrary tflitelib = () {
   if (Platform.isAndroid) {
-    return DynamicLibrary.open('libtensorflowlite_c.so');
+    try {
+      print('kienmtTest load dynamic');
+      return DynamicLibrary.open('libtensorflowlite_c.so');
+    } catch (_) {
+      try {
+        const MethodChannel _channel = MethodChannel('tflite_flutter_plugin');
+        String libDir = asyncMethod(_channel);
+        print('kienmtTest load dynamic with libdir');
+        var lib = libDir;
+        print('kienmtTest libdir $lib');
+        return DynamicLibrary.open('$lib/libtensorflowlite_c.so');
+      } catch (_) {
+        print('kienmtTest load dynamic with data');
+        final appIdAsBytes = File('/proc/self/cmdline').readAsBytesSync();
+        // app id ends with the first \0 character in here.
+        final endOfAppId = max(appIdAsBytes.indexOf(0), 0);
+        final appId = String.fromCharCodes(appIdAsBytes.sublist(0, endOfAppId));
+        print('kienmtTest appId $appId');
+        return DynamicLibrary.open(
+            '/data/data/$appId/lib/libtensorflowlite_c.so');
+      }
+    }
   } else if (Platform.isIOS) {
     return DynamicLibrary.process();
-  } else {    
+  } else {
     return DynamicLibrary.open(
-      Directory(Platform.resolvedExecutable).parent.path + '/blobs/${binaryName}'
-    );
+        Directory(Platform.resolvedExecutable).parent.path +
+            '/blobs/${binaryName}');
   }
 }();
+
+dynamic asyncMethod(MethodChannel _channel) async {
+  final result = await _channel.invokeMethod('getNativeLibraryDir');
+  return result;
+}
+
